4927584cb8c235b02c8195a7f2f4262759c7b9a4
AustinxEwell <austin.ewell86@gmail.com>
2021-10-06 16:20:10 -0600

    added voter routes;

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const apiRoutes = require('./routes/apiRoutes');
 const db = require('./db/connection');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
